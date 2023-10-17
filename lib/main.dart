401be1fba6e3294beba9dcb2374a3892f007efd5
MahmoudElmaghraby <mahmoudelmaghraby515@gmail.com>
2023-10-17 15:16:10 +0300

    syntax update

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,13 @@
-import 'dart:io';
-
 import 'package:auth_mobile_app/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  //runApp(const MyApp());
-
-  print("Enter your name?");
-  // Reading name of the Geek
-  String? name = stdin.readLineSync(); // null safety in name string
-
-  // Printing the name
-  print("Hello, $name! \nWelcome to GeeksforGeeks!!");
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
